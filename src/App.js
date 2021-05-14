a4b83ec50dc0e0ed2e8e6bd4d22f0361265e2be1
mayurlalwani <mayur_lalwani4@ymail.com>
2021-05-14 14:36:08 +0530

    Created homepage and directory file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Header from './components/Header';
+import Homepage from './pages/homepage';
+import './default.scss';
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Homepage />
+      </div>
+    </div>
+  );
 }
 
 export default App;

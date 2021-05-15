ee9079776248a044dd5215108eecdba96886b15f
mayurlalwani <mayur_lalwani4@ymail.com>
2021-05-15 14:46:14 +0530

    Stored logged in user data in firebase firestore

--- src/App.js
+++ src/App.js
@@ -1,34 +1,86 @@
+import React, { Component } from 'react';
 import Homepage from './pages/homepage';
 import Registration from './pages/Registration';
-import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import './default.scss';
+import HomepageLayout from './layouts/HomepageLayout';
 import MainLayout from './layouts/MainLayout';
+import { auth, handleUserProfile } from './firebase/utils';
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <MainLayout>
-              <Homepage />
-            </MainLayout>
-          )}
-        />
-        <Route
-          path="/registration"
-          render={() => (
-            <MainLayout>
-              <Registration />
-            </MainLayout>
-          )}
-        />
-      </Switch>
-    </div>
-  );
+const initialState = {
+  currentUser: null,
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState,
+    };
+  }
+
+  authListener = null;
+  componentDidMount() {
+    this.authListener = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await handleUserProfile(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+        });
+      }
+      this.setState({ ...initialState });
+    });
+  }
+
+  componentWillUnmount() {
+    this.authListener();
+  }
+
+  render() {
+    const { currentUser } = this.state;
+    return (
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <HomepageLayout currentUser={currentUser}>
+                <Homepage />
+              </HomepageLayout>
+            )}
+          />
+          <Route
+            path="/registration"
+            render={() => (
+              <MainLayout currentUser={currentUser}>
+                <Registration />
+              </MainLayout>
+            )}
+          />
+          <Route
+            path="/login"
+            render={() =>
+              currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <MainLayout currentUser={currentUser}>
+                  <Login />
+                </MainLayout>
+              )
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -4,7 +4,7 @@ import Footer from './../components/Footer';
 const MainLayout = (props) => {
   return (
     <div className="fullHeight">
-      <Header />
+      <Header {...props} />
       <div className="main">{props.children}</div>
       <Footer />
     </div>
